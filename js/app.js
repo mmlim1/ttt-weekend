69ae2bf74197385793b293bd96231f893174b84e
mmlim1 <michaelmlim1@gmail.com>
2022-06-02 17:12:01 -0700

    reset button added and updates

--- js/app.js
+++ js/app.js
@@ -10,9 +10,6 @@ const winningCombos = [
   [2, 4, 6]
 ]
 
-
-
-
 /*---------------------------- Variables (state) ----------------------------*/
 let board //state of the sqaures on the board
 let turn //track whose turn it is
@@ -22,6 +19,7 @@ let winner //represent if anyone has won yet, or if a tie has occured
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.board-div')
 const messageEl = document.querySelector('h2')
+const resetBtnEl = document.querySelector('button')
 //console.log(squareEls)
 console.log(messageEl.textContent)
 
@@ -31,6 +29,7 @@ console.log(messageEl.textContent)
 squareEls.forEach(function(square) {
   square.addEventListener('click', handleClick)
 })
+resetBtnEl.addEventListener('click', init)
 
 
 
@@ -56,12 +55,12 @@ function render() {
     }
   })
     
-    if (winner === null) {
-      messageEl.textContent = `It is player ${turn}'s turn!`
-    } else if (winner === 'T') {
-      messageEl.textContent = `It's a tie!`
-    } else {
-      messageEl.textContent = `Player ${winner} won!`
+  if (winner === null) {
+    messageEl.textContent = `It is ${turn === 1 ? 'X' : 'O'}'s turn!`
+  } else if (winner === 'T') {
+    messageEl.textContent = `It's a tie!`
+  } else if (winner === 1 || winner === -1) {
+    messageEl.textContent = `Player ${winner === 1 ? 'X' : 'O'} won!`
   }
 } 
 
@@ -74,22 +73,25 @@ function handleClick(evt) {
   turn = turn * -1 
   getWinner()
   render()
-console.log(sqIdx)
-console.log(board[sqIdx])
-console.log(turn)
+
 }
 
 function getWinner() {  
   for (let i=0; i < winningCombos.length; i++) {
       let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]
+      console.log(sum)
       if (sum === 3) {
-          winner === 1
+        winner = 1  
+        return
       } else  if (sum === -3) {
-          winner === -1
+        winner = -1
+        return
       } else if (board.includes(null) === false) {
-          winner === 'T'
-      }
+        winner = 'T'
+        return
+      } else {
+        winner = null
+      } 
+    
   }
-  return null
 }
-
